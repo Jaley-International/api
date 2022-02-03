a078a44560b3c4a1e9cbd7257a50c1c4914aaa8d
Yohann THEPAUT <contact@ythepaut.com>
2022-02-03 16:40:58 +0000

    Add API port to .env

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PEC_API_PORT);
 }
 bootstrap();
