6719779e40c679b80a84c6cd21ffe5673c165ae9
Jasmine Jiang <headlessnun@gmail.com>
2022-03-17 22:33:16 +0000

    Save node sharing details sent from client and log the activity

--- src/share/share.controller.ts
+++ src/share/share.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { ShareService } from './share.service';
+import { Request } from 'express';
+import { getSession, getSessionUser } from '../utils/session';
+import { res } from '../utils/communication';
+import { ShareNodeDto } from './share.dto';
 
 @Controller('share')
 export class ShareController {
   constructor(private shareService: ShareService) {}
+
+  /**
+   * Shares a node with another user internally.
+   */
+  @Post()
+  async shareNode(@Req() req: Request, @Body() body: ShareNodeDto) {
+    const curUser = await getSessionUser(req);
+    const session = await getSession(req);
+    await this.shareService.share(curUser, session, body);
+    return res('Successfully shared a node.', {});
+  }
 }
