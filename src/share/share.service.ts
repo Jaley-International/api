1af01a14d4b2c7a3b311506d2c1467a1d77f034e
zarvoir <agungbanar@gmail.com>
2022-04-03 22:12:38 +0100

    fix #47 , added check for duplicate share object and invalid share object

--- src/share/share.service.ts
+++ src/share/share.service.ts
@@ -8,7 +8,8 @@ import { LogService } from '../log/log.service';
 import { UserService } from '../user/user.service';
 import { FilesystemService } from '../filesystem/filesystem.service';
 import { ActivityType } from '../log/log.entity';
-import { NodeType } from '../filesystem/filesystem.entity';
+import { NodeType, Node } from '../filesystem/filesystem.entity';
+import { err, Status } from '../utils/communication';
 
 @Injectable()
 export class ShareService {
@@ -32,7 +33,13 @@ export class ShareService {
 
     const node = await this.fileService.findOne({
       where: { id: body.nodeId },
-      relations: ['parent', 'owner'],
+      relations: [
+        'parent',
+        'owner',
+        'shares',
+        'shares.sender',
+        'shares.recipient',
+      ],
     });
 
     const share = new Share();
@@ -41,19 +48,40 @@ export class ShareService {
     share.sender = curUser;
     share.recipient = recipient;
     share.node = node;
-    await this.shareRepo.save(share);
-
-    await this.logService.createNodeLog(
-      node.type === NodeType.FILE
-        ? ActivityType.FILE_SHARING
-        : ActivityType.FOLDER_SHARING,
-      node,
-      node.parent,
-      null,
-      curUser,
-      session,
-      node.owner,
-      recipient,
-    );
+    if (isShareValid(node, share.recipient, share.sender)) {
+      await this.shareRepo.save(share);
+      await this.logService.createNodeLog(
+        node.type === NodeType.FILE
+          ? ActivityType.FILE_SHARING
+          : ActivityType.FOLDER_SHARING,
+        node,
+        node.parent,
+        null,
+        curUser,
+        session,
+        node.owner,
+        recipient,
+      );
+    } else {
+      throw err(Status.ERROR_INVALID_SHARE, 'Share is invalid');
+    }
   }
+}
+
+/**
+ * check if the current node are valid to be shared by doing the following
+ * check if sender and recipient are the same if it is return false
+ * check the shares array objects and check each the recipient and sender
+ * if it has the same sender and recipient then return false
+ */
+function isShareValid(node: Node, sender: User, recipient: User): boolean {
+  if (sender === recipient) {
+    return false;
+  }
+  node.shares.forEach((share) => {
+    if (share.sender === sender && share.recipient === recipient) {
+      return false;
+    }
+  });
+  return true;
 }
